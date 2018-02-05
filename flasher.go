9410cb124ae1dd85e9736de0ddba45754032ad9b
Jacob Lewallen <jlewallen@gmail.com>
2018-02-05 12:50:14 -0800

    Flag to reopen after tail inactivity. Also close port correctly.

--- flasher.go
+++ flasher.go
@@ -21,6 +21,7 @@ type configuration struct {
 	Tail           bool
 	TailAppend     string
 	TailInactivity int
+	TailReopen     bool
 }
 
 func searchForTools(config *configuration) string {
@@ -52,30 +53,37 @@ func searchForTools(config *configuration) string {
 	panic(fmt.Sprintf("Unable to find tools, looked in %v", candidates))
 }
 
-func echoSerial(config *configuration, c *chan bool) {
+func openSerial(config *configuration) (serial.Port, error) {
 	mode := &serial.Mode{
 		BaudRate: 115200,
 	}
 	port, err := serial.Open(config.Port, mode)
 	if err != nil {
-		log.Fatalf("Unable to open %s (%v)", config.Port, err)
+		log.Fatalf("Unable to open %s: %v", config.Port, err)
 	}
+
+	return port, nil
+}
+
+func echoSerial(config *configuration, port serial.Port, c *chan bool) {
+	defer port.Close()
 
 	var file *os.File
 	if config.TailAppend != "" {
 		log.Printf("Logging to %s", config.TailAppend)
-		file, err = os.OpenFile(config.TailAppend, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		file, err := os.OpenFile(config.TailAppend, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 		if err != nil {
-			log.Fatalf("Unable to open %s (%v)", config.TailAppend, err)
+			log.Fatalf("Unable to open %s: %v", config.TailAppend, err)
 		}
 		defer file.Close()
 	}
 
-	buff := make([]byte, 100)
+	buff := make([]byte, 256)
+
 	for {
 		n, err := port.Read(buff)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Error reading: %v", err)
 			break
 		}
 		if n == 0 {
@@ -87,6 +95,8 @@ func echoSerial(config *configuration, c *chan bool) {
 		fmt.Printf("%v", sanitized)
 		file.WriteString(sanitized)
 	}
+
+	log.Printf("Tailing done.")
 }
 
 func main() {
@@ -100,6 +110,7 @@ func main() {
 	flag.BoolVar(&config.Tail, "tail", false, "show serial")
 	flag.StringVar(&config.TailAppend, "append", "", "append tail to file")
 	flag.IntVar(&config.TailInactivity, "tail-inactivity", 0, "inactive time until quitting tail")
+	flag.BoolVar(&config.TailReopen, "tail-reopen", false, "tail again after inactivity or file loss")
 	flag.Parse()
 
 	if config.Binary == "" && !config.Tail {
@@ -147,37 +158,52 @@ func main() {
 	if config.Tail {
 		time.Sleep(1 * time.Second)
 
-		if _, err := os.Stat(config.Port); os.IsNotExist(err) {
-			log.Printf("Port '%s' disappeared, scanning...", config.Port)
-			config.Port = pd.discover()
-			if config.Port == "" {
-				log.Fatalf("Error: Unable to find port to tail.")
-			}
-		}
-
-		ch := make(chan bool)
-		go echoSerial(&config, &ch)
-
-		go func() {
-			for {
-				time.Sleep(1 * time.Second)
-				ch <- false
-			}
-		}()
-
-		previous := time.Now()
 		for {
-			data := <-ch
-			if data {
-				previous = time.Now()
+			if _, err := os.Stat(config.Port); os.IsNotExist(err) {
+				log.Printf("Port '%s' disappeared, scanning...", config.Port)
+				config.Port = pd.discover()
+				if config.Port == "" {
+					log.Fatalf("Error: Unable to find port to tail.")
+				}
 			}
 
-			if config.TailInactivity > 0 {
-				ago := time.Duration(-config.TailInactivity) * time.Second
-				to := time.Now().Add(ago)
-				if previous.Before(to) {
-					break
+			ch := make(chan bool)
+
+			port, err := openSerial(&config)
+			if err != nil {
+				log.Fatalf("Error: Unable to open port: %v", err)
+			}
+
+			go echoSerial(&config, port, &ch)
+
+			go func() {
+				for {
+					time.Sleep(1 * time.Second)
+					ch <- false
 				}
+			}()
+
+			previous := time.Now()
+
+			for {
+				data := <-ch
+				if data {
+					previous = time.Now()
+				}
+
+				if config.TailInactivity > 0 {
+					ago := time.Duration(-config.TailInactivity) * time.Second
+					to := time.Now().Add(ago)
+					if previous.Before(to) {
+						port.Close()
+						log.Printf("Tail inactive!")
+						break
+					}
+				}
+			}
+
+			if !config.TailReopen {
+				break
 			}
 		}
 	}
